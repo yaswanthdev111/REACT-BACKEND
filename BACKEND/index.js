a5574a7ee0f71a71277fa0ec0f7e9028fa434175
= <=>
2025-02-27 16:29:53 +0530

    local

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,33 +1,54 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/ConnectDB')
-const router = require('./routes/index')
-const cookiesParser = require('cookie-parser')
-const { app, server } = require('./socket/index')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/ConnectDB");
+const router = require("./routes/index");
+const cookiesParser = require("cookie-parser");
+const { app, server } = require("./socket/index");
 
 // const app = express()
-console.log(process.env.FRONTEND_URL)
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookiesParser())
+console.log(process.env.FRONTEND_URL);
+// app.use(cors({
+//     origin : process.env.FRONTEND_URL,
+//     credentials : true
+// }))
 
-const PORT = process.env.PORT || 8080
+const PORT = 3010;
 
-app.get('/',(request,response)=>{
-    response.json({
-        message : "Server running at " + PORT
-    })
-})
+const corsWhitelist = [
+  "https://youtube-app-sanju-vert.vercel.app",
+  "https://react-frontend-d8i9.vercel.app/",
+  "http://localhost:3000/",
+  "https://prototype-verceldeployment-client.vercel.app",
+  "https://prototype-verceldeployment-server.vercel.app",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (corsWhitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// app.use(cookiesParser());
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Server running at " + PORT,
+  });
+});
 
 //api endpoints
-app.use('/api',router)
+app.use("/api", router);
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
-})
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log("server running at " + PORT);
+  });
+});
